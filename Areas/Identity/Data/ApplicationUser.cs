edf7ae8609f568ea5328dceb9495ef722baaa11a
Técnico em Des. de Sistemas <aluno2T1BT@educ602.local>
2022-09-16 17:23:00 -0300

    user feito

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace User_Empresa.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    
+    [Required]
+    [EmailAddress(ErrorMessage = "Email Inválido!")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Insira seu RA!")]
+    public int RA { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public int Telefone { get; set; }
+
+}
+
---
+++ Migrations/20220916195250_testando.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User_Empresa.Migrations
+{
+    public partial class testando : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RA",
+                table: "AspNetUsers",
+                type: "int",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Telefone",
+                table: "AspNetUsers",
+                type: "int",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RA",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
